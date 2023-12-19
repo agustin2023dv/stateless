aaa1483dd78a461e68242f8d69ccd003165cb8a6
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2023-12-19 13:34:11 +0100

    add: Basic implementation of firebase auth for sign up

---
+++ client/src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDjLJidg5gErXrviJloYzfUZ_8_r1qS0LA",
+  authDomain: "stateless-5e843.firebaseapp.com",
+  projectId: "stateless-5e843",
+  storageBucket: "stateless-5e843.appspot.com",
+  messagingSenderId: "831049862396",
+  appId: "1:831049862396:web:32bb60d45cb440c9d49b79"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,51 +1,97 @@
 import '../assets/styles/Styles.css';
 
-function SignUp(){
+import { auth } from '../firebase';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { useState } from 'react';
 
-return(
+function SignUp() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+
+    const emailValue = email;
+    const passwordValue = password;
+
+    try {
+      // Create a user account in Firebase
+      await createUserWithEmailAndPassword(auth, emailValue, passwordValue);
+
+      // If registration is successful, you can perform actions here, such as redirecting the user to another page.
+      console.log('Registration successful');
+
+    } catch (error) {
+      // Handle registration errors
+      console.error('Error registering:', error.message);
+    }
+  };
+
+  return (
     <>
-        <div id='header-signup'>
-            <div id="got-account">
-                <h4>Already got an account?</h4><a href="/sign-up">Sign in!</a>
-            </div>
+      <div id='header-signup'>
+        <div id="got-account">
+          <h4>Already got an account?</h4><a href="/sign-up">Sign in!</a>
         </div>
-        
-        <div id="sign-up">
-            
-            <div id="title-sup">
-                <h1>Sign Up </h1>
-                <h4>It's quick and easy!</h4>
-            </div>
-            
+      </div>
 
-            <form>
-                <div class="form-group">
-                    <label for="fusername">Username</label>
-                    <input type="text" id="fusername" name="fusername" />
-                </div>
-            
-                <div class="form-group">
-                    <label for="email">Email address</label>
-                    <input type="email" id="email" name="email" />
-                </div>
-                <div class="form-group">
-                    <label for="lpassword">Password</label>
-                    <input type="password" id="lpassword" name="lpassword" />
-                </div>
-                <div class="form-group">
-                    <label for="lcpassword">Confirm password</label>
-                    <input type="password" id="lcpassword" name="lcpassword" />
-                </div>
+      <div id="sign-up">
 
-                <input type="submit" value="Create account" />
-            </form>
-
+        <div id="title-sup">
+          <h1>Sign Up </h1>
+          <h4>It's quick and easy!</h4>
         </div>
-    
-    
+
+        <form onSubmit={handleSignUp}>
+          <div className="form-group">
+            <label htmlFor="fusername">Username</label>
+            <input
+              type="text"
+              id="fusername"
+              name="fusername"
+              placeholder='Enter a username'
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="email">Email address</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder='Enter your email address'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="lpassword">Password</label>
+            <input
+              type="password"
+              id="lpassword"
+              name="lpassword"
+              placeholder='Enter a password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="lcpassword">Confirm password</label>
+            <input
+              type="password"
+              id="lcpassword"
+              name="lcpassword"
+              placeholder='Enter the same password again'
+            />
+          </div>
+
+          <input type="submit" value="Create account" />
+        </form>
+      </div>
     </>
-    
-);
+  );
 }
 
 export default SignUp;
