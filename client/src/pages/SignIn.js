fa6bb9904d3312bba8c25ae502d6566b323b088b
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2023-12-23 12:44:49 +0100

    feat: Login with either email and password or username and password

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -2,27 +2,86 @@ import '../assets/styles/Styles.css';
 import statelessLogo75 from '../assets/img/statelessLogo75.png';
 import { useState } from 'react';
 import { signInWithEmailAndPassword } from 'firebase/auth';
-import { auth } from '../firebase'; // Import auth from your firebase.js file
+import { auth,db} from '../firebase'; // Import auth from your firebase.js file
 import { useHistory } from 'react-router-dom';
+import { getDocs,query,collection,where } from 'firebase/firestore';
+
 
 function SignIn() {
   const [userID, setUserID] = useState('');
   const [password, setPassword] = useState('');
   const history = useHistory();
 
-  const handleSignIn = async (e) => { // Removed unnecessary "type" argument
-    e.preventDefault();
 
+  const handleSignIn = async (e) => {
+    e.preventDefault();
+  
+    const userIDValue = userID;
     try {
-      // Use the state variables instead of accessing e.target
-      await signInWithEmailAndPassword(auth, userID, password);
-      // Redirect to the home page on successful login
-      history.push('/');
+      const userQuery = query(collection(db, 'user'), where('username', '==', userIDValue));
+      const userQuerySnapshot = await getDocs(userQuery);
+  
+      const userDocument = userQuerySnapshot.docs[0]?.data(); // Usar "?." para evitar errores si no hay resultados
+      const userEmail = userDocument?.email;
+  
+      if (userEmail) {
+        // Intenta iniciar sesión con el correo electrónico
+        const userCredential = await signInWithEmailAndPassword(auth, userEmail, password);
+        if (userCredential) {
+          // La autenticación fue exitosa, redirige al usuario
+          history.push('/');
+        } else {
+          // La autenticación falló, muestra un mensaje de error
+          alert('Authentication failed.');
+        }
+      } else {
+        // Si no se encontró un correo electrónico, intenta iniciar sesión con el nombre de usuario
+        const userCredential = await signInWithEmailAndPassword(auth, userID, password);
+        if (userCredential) {
+          // La autenticación fue exitosa, redirige al usuario
+          history.push('/');
+        } else {
+          // La autenticación falló, muestra un mensaje de error
+          alert('Authentication failed.');
+        }
+      }
     } catch (err) {
       // Handle authentication errors
       alert(err.message);
     }
-  }
+  };
+  
+
+/*  const handleSignIn = async (e) => { 
+    e.preventDefault();
+
+    const userIDValue = userID;
+    try {
+      
+      const userQuery = query(collection(db, 'user'), where('username', '==', userIDValue));
+      const userQuerySnapshot = await getDocs(userQuery);
+
+      const userDocument = userQuerySnapshot.docs[0].data();
+      const userEmail = userDocument.email;
+
+      
+      // Use the state variables instead of accessing e.target
+      if(await signInWithEmailAndPassword(auth, userID, password)){
+        // Redirect to the home page on successful login
+        history.push('/');
+      } 
+      else if(userEmail){ //Here we try to login with the username 
+
+        await signInWithEmailAndPassword(auth, userEmail, password);
+        // Redirect to the home page on successful login
+        history.push('/');
+      }
+      
+    } catch (err) {
+      // Handle authentication errors
+      alert(err.message);
+    }
+  }*/
 
   return (
     <div id="container">
@@ -67,7 +126,7 @@ function SignIn() {
 
         <div id='buttons-su'>
           <button id='button-su' type="submit">
-            Sign in
+            Login
           </button>
 
           <button type="button" className="login-with-google-btn">
